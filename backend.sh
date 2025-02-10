cbe792e0dcae847c3f263dccb064a18713bb3028
akram <akram.khan200@gmail.com>
2025-02-10 20:08:13 +0530

    my changes 12121233

---
+++ backend.sh
@@ -0,0 +1,23 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+
+dnf install nodejs -y
+
+ cp backend.service /etc/systemd/system/backend.service
+useradd expense
+
+mkdir /app
+
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
+cd /app
+unzip /tmp/backend.zip
+
+cd /app
+npm install
+
+systemctl daemon-reload
+systemctl enable backend
+systemctl start backend
+
+mysql -h 172.31.32.100  -uroot -pExpenseApp@1 < /app/schema/backend.sql
+
