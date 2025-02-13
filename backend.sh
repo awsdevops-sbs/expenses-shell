2cb82d78fb671553b00ebbc8a2c1f302ff9340ea
akram <akram.khan200@gmail.com>
2025-02-13 17:31:32 +0530

    version 1.1.9

--- backend.sh
+++ backend.sh
@@ -11,44 +11,44 @@ fi
 
 print "Disable default NodeJS Version Module"
 dnf module disable nodejs -y &>>$LOG
-Check_Status $?
+check_status $?
 
 print "Enable NodeJS module for V20"
 dnf module enable nodejs:20 -y &>>$LOG
-Check_Status $?
+check_status $?
 
 print "Install NodeJS"
 dnf install nodejs -y &>>$LOG
-Check_Status $?
+check_status $?
 
 print "Adding Application User"
 id expense &>>$LOG
 if [ $? -ne 0 ]; then
   useradd expense &>>$LOG
 fi
-Check_Status $?
+check_status $?
 
 print "Copy Backend Service file"
 cp backend.service /etc/systemd/system/backend.service &>>$LOG
-Check_Status $?
+check_status $?
 
 App_PreReq
 
 print "Download NodeJS Dependencies"
 cd /app &>>$LOG
 npm install &>>$LOG
-Check_Status $?
+check_status $?
 
 print "Start Backend Service"
 systemctl daemon-reload &>>$LOG
 systemctl enable backend &>>$LOG
 systemctl start backend &>>$LOG
-Check_Status $?
+check_status $?
 
 print "Install MySQL Client"
 dnf install mysql -y &>>$LOG
-Check_Status $?
+check_status $?
 
 print "Load Schema"
 mysql -h mysql-dev.awsdevops.sbs -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOG
-Check_Status $?
+check_status $?
