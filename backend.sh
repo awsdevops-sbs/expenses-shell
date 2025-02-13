7438717b83da67fbe1056e27b8dce95390c7c31c
akram <akram.khan200@gmail.com>
2025-02-13 16:57:39 +0530

    Revert "version 1.1.6"
    This reverts commit 58da9c0f1cbfceb617cfb709b4c59f871f756e34.

--- backend.sh
+++ backend.sh
@@ -13,44 +13,44 @@ fi
 
 print "Disable default NodeJS Version Module"
 dnf module disable nodejs -y &>>$LOG
-check_status $?
+Check_Status $?
 
 print "Enable NodeJS module for V20"
 dnf module enable nodejs:20 -y &>>$LOG
-check_status $?
+Check_Status $?
 
 print "Install NodeJS"
 dnf install nodejs -y &>>$LOG
-check_status $?
+Check_Status $?
 
 print "Adding Application User"
 id expense &>>$LOG
 if [ $? -ne 0 ]; then
   useradd expense &>>$LOG
 fi
-check_status $?
+Check_Status $?
 
 print "Copy Backend Service file"
 cp backend.service /etc/systemd/system/backend.service &>>$LOG
-check_status $?
+Check_Status $?
 
 App_PreReq
 
 print "Download NodeJS Dependencies"
 cd /app &>>$LOG
 npm install &>>$LOG
-check_status $?
+Check_Status $?
 
 print "Start Backend Service"
 systemctl daemon-reload &>>$LOG
 systemctl enable backend &>>$LOG
 systemctl start backend &>>$LOG
-check_status $?
+Check_Status $?
 
 print "Install MySQL Client"
 dnf install mysql -y &>>$LOG
-check_status $?
+Check_Status $?
 
 print "Load Schema"
 mysql -h mysql-dev.awsdevops.sbs -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOG
-check_status $?
+Check_Status $?
