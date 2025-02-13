000830c3ed06511f6df97062ce24f33d34b00eb9
akram <akram.khan200@gmail.com>
2025-02-13 15:13:01 +0530

    version 1.1.1

--- backend.sh
+++ backend.sh
@@ -1,47 +1,56 @@
 source common.sh
 
-print "disable nodejs"
-dnf module disable nodejs -y &>>/tmp/exp.log
- echo $?
 
- print "enabling nodejs version 20"
-dnf module enable nodejs:20 -y &>>/tmp/exp.log
- echo $?
+mysql_root_password=$1
+app_dir=/app
+component=backend
 
- print "install nodejs"
-dnf install nodejs -y &>>/tmp/exp.log
- echo $?
+# If password is not provided then we will exit
+if [ -z "${mysql_root_password}" ]; then
+  echo Input Password is missing.
+  exit 1
+fi
 
- print "copying backend service"
- cp backend.service /etc/systemd/system/backend.service &>>/tmp/exp.log
-  echo $?
-useradd expense
+print "Disable default NodeJS Version Module"
+dnf module disable nodejs -y &>>$LOG
+Check_Status $?
 
-mkdir /app &>>/tmp/exp.log
- echo $?
+print "Enable NodeJS module for V20"
+dnf module enable nodejs:20 -y &>>$LOG
+Check_Status $?
 
- print "downloading backend components"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/exp.log
- echo $?
-cd /app &>>/tmp/exp.log
+print "Install NodeJS"
+dnf install nodejs -y &>>$LOG
+Check_Status $?
 
-print "extracting backend components"
-unzip /tmp/backend.zip
- echo $?
-cd /app &>>/tmp/exp.log
- echo $?
- print "installing nodejs"
-npm install
- echo $?
- print "reloading DB"
-systemctl daemon-reload &>>/tmp/exp.log
- echo $?
- print "enable backend service"
-systemctl enable backend &>>/tmp/exp.log
- echo $?
+print "Adding Application User"
+id expense &>>$LOG
+if [ $? -ne 0 ]; then
+  useradd expense &>>$LOG
+fi
+Check_Status $?
 
- print "start backend service"
-systemctl start backend &>>/tmp/exp.log
- echo $?
-mysql -h 172.31.32.100  -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>/tmp/exp.log
- echo $?
+print "Copy Backend Service file"
+cp backend.service /etc/systemd/system/backend.service &>>$LOG
+Check_Status $?
+
+App_PreReq
+
+print "Download NodeJS Dependencies"
+cd /app &>>$LOG
+npm install &>>$LOG
+Check_Status $?
+
+print "Start Backend Service"
+systemctl daemon-reload &>>$LOG
+systemctl enable backend &>>$LOG
+systemctl start backend &>>$LOG
+Check_Status $?
+
+print "Install MySQL Client"
+dnf install mysql -y &>>$LOG
+Check_Status $?
+
+print "Load Schema"
+mysql -h mysql-dev.awsdevops.sbs -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOG
+Check_Status $?
